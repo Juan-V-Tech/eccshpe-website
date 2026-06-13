0785cd6f438edf49e13cde6b2668fb7f394854a0
Juan-V-Tech <165835806+Juan-V-Tech@users.noreply.github.com>
2026-06-12 23:07:36 -0700

    updated urls

--- src/pages/alumni.jsx
+++ src/pages/alumni.jsx
@@ -14,7 +14,7 @@ const exLeadershipAlumni = [
     graduationYear: 2026,
     roleInShpe: 'Co-President',
     currentPosition: 'Structural Engineering at UCSD',
-    linkedinUrl: 'https://www.linkedin.com/in/angel-flores/',
+    linkedinUrl: 'https://www.linkedin.com/in/angel-flores-844963216/',
     img: '/headshot/AngelFlores.jpeg'
   },
   {
@@ -22,7 +22,7 @@ const exLeadershipAlumni = [
     graduationYear: 2026,
     roleInShpe: 'Secretary',
     currentPosition: 'Computer Engineering at UCI',
-    linkedinUrl: 'https://www.linkedin.com/in/destiny-mungia/',
+    linkedinUrl: 'https://www.linkedin.com/in/destinymungia/',
     img: '/headshot/DestinyMunguia_008.jpg'
   },
   {
@@ -30,7 +30,7 @@ const exLeadershipAlumni = [
     graduationYear: 2026,
     roleInShpe: 'Treasurer',
     currentPosition: 'Structural Engineering at UCSD',
-    linkedinUrl: 'https://www.linkedin.com/in/miguel-gonzalez/',
+    linkedinUrl: 'https://www.linkedin.com/in/miguel-gonzalez-46b94934a/',
     img: '/headshot/Miguel.jpeg'
   }
 ];
