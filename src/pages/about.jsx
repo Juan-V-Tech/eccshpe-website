8337880a242259433252e9b87d57aed09205ab44
Juan-V-Tech <165835806+Juan-V-Tech@users.noreply.github.com>
2026-09-24 19:25:26 -0700

    force push lol

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
           We are the El Camino College student chapter of the Society of Hispanic Professional Engineers, dedicated to empowering Hispanic and other underrepresented students in STEM. Our community is built on fostering academic success and creating pathways to thriving STEM careers for our members.
         </p>
         <p style={{ fontSize: '1.05rem', maxWidth: 860, lineHeight: 1.5 }}>
-          Through national conferences, engineering workshops, local competitions, and volunteer opportunities, we launch our members into becoming the next generation of engineers. With the support of industry partners, campus connections, and our national network, we provide high-quality opportunities for our members to grow academically, professionally, and personally.
+          Through national conferences, engineering workshops, local competitions, and volunteer opportunities, we launch our members into becoming the next generation of engineers. With the support of industry partners, campus connections, and our national network, we provide high-quality opportunities for our members to grow academically, professionally, and personally..
         </p>
 
       </div>
